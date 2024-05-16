e0a6a67268e2e75d302332fb3d1bd182b725b35c
Louis <loui9583@stud.kea.dk>
2024-05-16 14:32:34 +0200

    a

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,13 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        // Use the Vercel adapter for Vercel deployment
+        adapter: adapter(),
+        // Add any other configurations here as needed
+    }
 };
 
 export default config;
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    server: {
-        host: true, // Automatically detect IP address for the server
-        port: 3000, // Default port during development (change as needed)
-    },
-    preview: {
-        port: 10000, // Ensure the preview uses the Render expected port
-        host: true // Listen on all IPs; necessary for Render environment
-    }
+	plugins: [sveltekit()]
 });
