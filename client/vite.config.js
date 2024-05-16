0b94ce7400afc5ef2ad565a82392b2ad348d89bc
Louis <loui9583@stud.kea.dk>
2024-05-16 13:46:54 +0200

    getting ready for deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    server: {
+        host: true, // Automatically detect IP address for the server
+        port: 3000, // Default port during development (change as needed)
+    },
+    preview: {
+        port: 10000, // Ensure the preview uses the Render expected port
+        host: true // Listen on all IPs; necessary for Render environment
+    }
 });
